48c7fe4f2f8024f303b8ce79c82d6e868da2b8aa
Carlos Rios <rioscarlosd@gmail.com>
2013-11-08 01:24:02 -0300

    accesing to string resources

--- src/com/example/examplesproject/MainActivity.java
+++ src/com/example/examplesproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.examplesproject;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -9,7 +10,13 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//savedInstanceState.putString("Answer", "Hola");
+		//String msg = (String)savedInstanceState.get("Answer"); 
+		//Log.i("Anwers", msg);
 		setContentView(R.layout.activity_main);
+		
+		String txt = this.getResources().getString(R.string.hello_world);
+		int an_Int = this.getResources().getInteger(R.integer.an_Int);
 	}
 
 	@Override
